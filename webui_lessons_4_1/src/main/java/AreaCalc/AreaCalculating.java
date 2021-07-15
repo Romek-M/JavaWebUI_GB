d20c7db77440056637d5ddf527b440f9701cbba2
romek <malaschenko_ra@mail.ru>
2021-07-15 11:59:48 +0300

    Практическое задание №4, ИСПРАВЛЕННОЕ!!!!

---
+++ webui_lessons_4_1/src/main/java/AreaCalc/AreaCalculating.java
@@ -0,0 +1,11 @@
+package AreaCalc;
+
+public class AreaCalculating {
+
+    public static void main(String[] args) {
+
+        Triangle triangle = new Triangle(3, 5 ,5);
+
+        System.out.println(triangle.triangleArea());
+    }
+}
